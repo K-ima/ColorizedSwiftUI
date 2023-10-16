983e694197bf3168b7dcc37df8ffeda22fa6335c
K-ima <bulgin.a.v@bk.ru>
2023-10-16 15:43:31 +0300

    Initial Commit

---
+++ Colorized/ColorizedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedApp.swift
+//  Colorized
+//
+//  Created by Kima on 16.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
